4e86764fdcc30503f0b9042c48157203ecd7c575
defmex <def>
2024-09-13 19:43:19 -0300

    agrege json pero esta para revision como se va a ocupar el programa en el backend

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,12 +1,13 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
-
+  
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
 export class InicioComponent {
-
+  
 }
