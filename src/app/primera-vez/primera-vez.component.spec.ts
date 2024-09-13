1a2ee0e81e0862432841c73a64c0392afa3cfb47
defmex <def>
2024-09-13 04:11:25 -0300

    cree base para las paginas en tema visual

--- src/app/primera-vez/primera-vez.component.spec.ts
+++ src/app/primera-vez/primera-vez.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { EncabezadoComponent } from './encabezado.component';
+import { PrimeraVezComponent } from './primera-vez.component';
 
-describe('EncabezadoComponent', () => {
-  let component: EncabezadoComponent;
-  let fixture: ComponentFixture<EncabezadoComponent>;
+describe('PrimeraVezComponent', () => {
+  let component: PrimeraVezComponent;
+  let fixture: ComponentFixture<PrimeraVezComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [EncabezadoComponent]
+      imports: [PrimeraVezComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(EncabezadoComponent);
+    fixture = TestBed.createComponent(PrimeraVezComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
---
+++ src/app/renovar/renovar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-renovar',
+  standalone: true,
+  imports: [],
+  templateUrl: './renovar.component.html',
+  styleUrl: './renovar.component.css'
+})
+export class RenovarComponent {
+
+}
